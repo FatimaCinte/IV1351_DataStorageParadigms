9eaa9dbfb6743b1afb33883940840b19eeb0eb40
FatimaCinte <125886038+FatimaCinte@users.noreply.github.com>
2023-11-28 10:31:35 +0100

    Update SoundgoodQueries.sql

--- SoundGood - task two/SoundgoodQueries.sql
+++ SoundGood - task two/SoundgoodQueries.sql
@@ -33,12 +33,14 @@ HAVING COUNT(*) > 2;
 
 /**Uppgift 4**/
 
-SELECT 
+SELECT
 TO_CHAR(d.date, 'Day') AS "Day", 
 genre AS "Genre", 
 COUNT(*)
 FROM lesson l
 LEFT JOIN date d ON l.date_id = d.date_id
 LEFT JOIN target_genre t ON l.target_genre_id = t.target_genre_id
-WHERE t.target_genre_id IS NOT NULL, EXTRACT(WEEK FROM d.date) = EXTRACT(WEEK FROM current_date) + 1 
-GROUP BY TO_CHAR(d.date, 'Day'), genre;
+WHERE t.target_genre_id IS NOT NULL AND EXTRACT(WEEK FROM d.date) = EXTRACT(WEEK FROM DATE '2023-02-10') + 1 
+GROUP BY TO_CHAR(d.date, 'Day'), genre, EXTRACT(DAY FROM d.date) 
+ORDER BY EXTRACT(DAY FROM d.date);
+

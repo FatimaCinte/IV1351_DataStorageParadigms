7853e5bcb5a60949004a17c60866bac64e3f0407
FatimaCinte <125886038+FatimaCinte@users.noreply.github.com>
2023-12-03 16:57:45 +0100

    Update SoundgoodQueries.sql

--- SoundgoodQueries.sql
+++ SoundgoodQueries.sql
@@ -1,15 +1,15 @@
 /*  IV1351 Data Storage Paradigms, project - Fatima Mohammad Ali, Mehrdad Vafaee  */
 
 /**Query 1**/
-SELECT TO_CHAR(d.date, 'Month') AS month, 
-		COUNT(l.lesson_id) AS total,
-		COUNT(CASE WHEN l.max_nr_of_students = '1' THEN 1 END) AS indv,
-		COUNT (CASE WHEN l.max_nr_of_students > '1' AND l.target_genre_id IS NULL THEN 1 END) AS grp,
-		COUNT (CASE WHEN l.target_genre_id IS NOT NULL THEN 1 END) AS ens
+SELECT TO_CHAR(d.date, 'Month') AS "Month", 
+		COUNT(l.lesson_id) AS "Total",
+		COUNT(CASE WHEN l.max_nr_of_students = '1' THEN 1 END) AS "Individual",
+		COUNT (CASE WHEN l.max_nr_of_students > '1' AND l.target_genre_id IS NULL THEN 1 END) AS "Group",
+		COUNT (CASE WHEN l.target_genre_id IS NOT NULL THEN 1 END) AS "Ensemble"
 FROM lesson AS l INNER JOIN date AS d ON l.date_id = d.date_id
 WHERE EXTRACT(YEAR FROM d.date) = 2023
 GROUP BY TO_CHAR(d.date,'Month')
-ORDER BY month;
+ORDER BY "Month";
 
 SELECT TO_CHAR(date.date, 'Month') AS "Month", 
 COUNT(*) AS "Total", 
